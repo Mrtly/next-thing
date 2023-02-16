1d8c1efab65bbb3320bfacdc42af17100efff0d6
Myrtle <myrtle.tzouma@gmail.com>
2023-02-16 11:52:51 -0500

    extract routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import TopNav from './components/topNav';
-import Textinput from './components/textinput';
-import Button from './components/button'
-import Checkbox from './components/checkbox'
-import Card from './components/card'
 import SideNav from './components/sideNav';
-import Modal from './components/modal';
-import Tooltip from './components/tooltip';
-import AllTheThings from './pages/allTheThings';
-import D3Bar from './samples/d3BarSample';
-import D3Line from './samples/d3LineSample';
-import D3Scatter from './samples/d3ScatterSample';
-import D3Map from './samples/d3MapSample';
 
-import { Route, Routes } from 'react-router-dom'
-import LeafletIss from './samples/leafletSample';
+import Router from "./routes";
 
 export default function App() {
   
@@ -23,20 +11,7 @@ export default function App() {
       <div className="flex font-ubuntu">
         <SideNav/>
         <div className="w-full min-w-screen min-h-screen p-10 bg-matcha flex flex-col items-center justify-center">
-          <Routes>
-            <Route path="/" element={<AllTheThings/>}/>
-            <Route path="/components/input" element={<Textinput id="input1" label="Name" />}/>
-            <Route path="/components/button" element={<Button color="moss"/>}/>
-            <Route path="/components/checkbox" element={<Checkbox id="checkbox1" label="thing"/>}/>
-            <Route path="/components/card" element={<Card children={<div>a card</div>} />}/>
-            <Route path="/components/modal" element={<Modal visible={true} />}/>
-            <Route path="/components/tooltip" element={<Tooltip displayText="what is this?" tooltipText="tooltip example revealed!" />}/>
-            <Route path="/samples/d3-bar" element={<D3Bar />}/>
-            <Route path="/samples/d3-line" element={<D3Line />}/>
-            <Route path="/samples/d3-scatterplot" element={<D3Scatter />}/>
-            <Route path="/samples/d3-map" element={<D3Map />}/>
-            <Route path="/samples/leaflet-map" element={<LeafletIss />}/>
-          </Routes>
+          <Router/>
         </div>
       </div>
     </>
---
+++ src/routes.tsx
@@ -0,0 +1,93 @@
+import { Route, Routes } from 'react-router-dom'
+
+import Textinput from './components/textinput';
+import Button from './components/button'
+import Checkbox from './components/checkbox'
+import Card from './components/card'
+import Modal from './components/modal';
+import Tooltip from './components/tooltip';
+import AllTheThings from './pages/allTheThings';
+import D3Bar from './samples/d3BarSample';
+import D3Line from './samples/d3LineSample';
+import D3Scatter from './samples/d3ScatterSample';
+import D3Map from './samples/d3MapSample';
+import LeafletIss from './samples/leafletSample';
+
+interface routerType {
+  name: string;
+  path: string;
+  element: JSX.Element;
+}
+
+const routes: routerType[] = [
+  {
+    path: "",
+    element: <AllTheThings />,
+    name: "All The Things"
+  },
+  { 
+    path: "components/input", 
+    element: <Textinput id="input1" label="Name" />,
+    name: "Text Input"
+  },
+  { 
+    path: "components/button", 
+    element:<Button color="moss"/>,
+    name: "Button"
+  },
+  { 
+    path: "/components/checkbox", 
+    element: <Checkbox id="checkbox1" label="thing"/>,
+    name: "Checkbox"
+  },
+  { 
+    path: "components/card", 
+    element: <Card children={<div>a card</div>} />,
+    name: "Card"
+  },
+  { 
+    path: "components/modal", 
+    element: <Modal visible={true} />,
+    name: "Modal"
+  },
+  { 
+    path: "components/tooltip", 
+    element: <Tooltip displayText="what is this?" tooltipText="tooltip example revealed!" />,
+    name: "Tooltip"
+  },
+  { 
+    path: "samples/d3-ba",
+    element: <D3Bar />, 
+    name: "D3 Bar"
+  },
+  { 
+    path: "samples/d3-line",
+    element: <D3Line />, 
+    name: "D3 Line"
+  },
+  { 
+    path: "samples/d3-scatterplot",
+    element: <D3Scatter />, 
+    name: "D3 Scatterplot"
+  },
+  { 
+    path: "samples/d3-map",
+    element: <D3Map />, 
+    name: "D3 Map"
+  },
+  { 
+    path: "samples/leaflet-map", 
+    element: <LeafletIss />,
+    name: "Leaflet Map"
+  }
+];
+
+const Router = () => {
+  const pageRoutes = routes.map(({ path, name, element }: routerType) => {
+    return <Route key={name} path={`/${path}`} element={element} />;
+  });
+
+  return <Routes>{pageRoutes}</Routes>;
+};
+
+export default Router;
