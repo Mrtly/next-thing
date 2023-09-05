577ef1767ab03a4046055796d522a2953981596c
Myrtle <myrtle.tzouma@gmail.com>
2023-09-05 15:07:51 -0400

    restructure

--- app/components/sideNavLink.tsx
+++ app/components/sideNavLink.tsx
@@ -13,8 +13,8 @@ const pathname = usePathname();
   
   return (
     <Link href={props.to} 
-      className={pathname == props.to ? "text-bark px-1 lg:px-3 py-2 border-b border-matcha block w-max font-semibold underline" : 
-      "text-bark px-1 lg:px-3 py-2 border-b border-matcha block w-max hover:underline "}>
+      className={pathname == props.to ? "text-bark px-1 lg:px-3 py-2 block w-max font-semibold underline" : 
+      "text-bark px-1 lg:px-3 py-2 block w-max hover:underline "}>
       {props.displayText}
     </Link>
   );
